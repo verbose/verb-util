cffbf4e876b1c9f9ab056e9d8faba8415c575173
jonschlinkert <dev@sellside.com>
2014-11-11 22:02:11 -0500

    v0.1.2

--- verbfile.js
+++ verbfile.js
@@ -3,14 +3,22 @@
 var verb = require('verb');
 var escape = require('./lib/middleware/escape');
 
+// temporary, need to get tests setup
+
 verb.data('package.json');
 verb.data({foo: 'bar'});
 
 verb.before(/\.*/, escape.before(verb));
 verb.after(/\.*/, escape.after(verb));
 
-verb.task('default', function() {
-  verb.src('delims.md')
-    .pipe(verb.dest('./actual'));
+verb.task('tests', function() {
+  verb.src('test/fixtures/*.md')
+    .pipe(verb.dest('./test/actual'));
 });
 
+verb.task('readme', function() {
+  verb.src('.verb.md')
+    .pipe(verb.dest('./'));
+});
+
+verb.task('default', ['tests', 'readme']);
